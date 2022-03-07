4ae82a222ebad08874fbf9cf9ff5d1452a693d0e
shiva <shiva.nelikanti@gmail.com>
2022-03-07 16:12:34 +0530

    First Message

---
+++ src/main/java/NewBatchSeleniumDemo/HomePageLaunch/AboutUsPage.java
@@ -0,0 +1,10 @@
+package NewBatchSeleniumDemo.HomePageLaunch;
+
+public class AboutUsPage {
+	
+	public void newMethod() {
+		
+		System.out.println("The new metho is added");
+	}
+
+}
---
+++ src/main/java/NewBatchSeleniumDemo/HomePageLaunch/HomePage.java
@@ -0,0 +1,20 @@
+package NewBatchSeleniumDemo.HomePageLaunch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\eclipse-workspace\\HomePageLaunch\\chromedriver.exe");
+		WebDriver inputDropDown = new ChromeDriver();
+		inputDropDown.get("https://www.google.com/");
+		inputDropDown.manage().window().maximize();
+		inputDropDown.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Selenium course");
+		
+
+	}
+
+}
