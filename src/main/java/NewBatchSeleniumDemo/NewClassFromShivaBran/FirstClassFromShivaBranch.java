3b821689798dad9bf80270db452152095af5726d
Intelli Pride <intellipride@gmail.com>
2022-04-11 00:05:15 +0530

    Added newly from Intellij - adding Branch1 plus Branch2 changes

--- src/main/java/NewBatchSeleniumDemo/NewClassFromShivaBran/FirstClassFromShivaBranch.java
+++ src/main/java/NewBatchSeleniumDemo/NewClassFromShivaBran/FirstClassFromShivaBranch.java
@@ -1,6 +1,7 @@
 package NewBatchSeleniumDemo.NewClassFromShivaBran;
 
 public class FirstClassFromShivaBranch {
+    // this is from Branch1 -- without commiting Stash
     // this is been added from adding class from shiva branch
     // Normal for Commiting
 }
