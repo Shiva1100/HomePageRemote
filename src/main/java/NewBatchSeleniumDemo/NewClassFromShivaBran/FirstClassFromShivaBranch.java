4dc563d16c997e2067c43a045133a59ccac27c42
Intelli Pride <intellipride@gmail.com>
2022-04-10 23:52:34 +0530

    Added newly from Intellij - added 2nd comment

--- src/main/java/NewBatchSeleniumDemo/NewClassFromShivaBran/FirstClassFromShivaBranch.java
+++ src/main/java/NewBatchSeleniumDemo/NewClassFromShivaBran/FirstClassFromShivaBranch.java
@@ -2,4 +2,5 @@ package NewBatchSeleniumDemo.NewClassFromShivaBran;
 
 public class FirstClassFromShivaBranch {
     // this is been added from adding class from shiva branch
+    // Normal for Commiting
 }
